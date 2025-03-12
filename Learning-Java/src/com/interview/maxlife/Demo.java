25d2d35a19b3d110536df682b4512791124c1c34
AmitJ95 <49363678+AmitJ95@users.noreply.github.com>
2025-03-12 17:15:32 +0530

    updated with projects from local

---
+++ Learning-Java/src/com/interview/maxlife/Demo.java
@@ -0,0 +1,19 @@
+package com.interview.maxlife;
+import java.util.*;
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		List<String> l = new ArrayList<>();
+		l.add("a");
+		l.add("b");
+		l.add("c");
+		l.add("d");
+		l.add("e");
+		
+		l.forEach(e -> System.out.println(e));
+		
+	}
+
+}
